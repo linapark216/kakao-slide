2741cbab4ab56e0f0c369c67290fe5f2e1aa5a16
lina <orangepop9005@gmail.com>
2023-08-11 11:40:34 +0900

    제이쿼리 170.kakao-slide

---
+++ js/function.js
@@ -0,0 +1,48 @@
+$(function () {
+	const $navi = $('.navigation>ul>li>a');
+	const $slides = $('.slides-container');
+
+	let nowIdx = 0;
+
+	const move = () => {
+		$navi.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
+		$slides.animate({ left: -100 * nowIdx + '%' }, 600, 'easeOutSine');
+	};
+
+	$navi.on('click', function (evt) {
+		evt.preventDefault();
+
+		nowIdx = $navi.index(this);
+
+		// $slides.animate({ left: -100 * nowIdx + '%' }, 600, 'easeOutSine');
+		// $navi.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
+		// console.log(nowIdx);
+		move();
+	});
+	$('.slides>a.slide-prev').on('click', function (evt) {
+		evt.preventDefault();
+
+		/* 0 ->5, 1 2 3 4 5 -> -1  */
+
+		if (nowIdx > 0) {
+			nowIdx--;
+		} else {
+			nowIdx = 5;
+		}
+
+		move();
+	});
+
+	$('.slides>a.slide-next').on('click', function (evt) {
+		evt.preventDefault();
+
+		/* 0 1 2 3 4 -> +1, 5->0  */
+		if (nowIdx < 5) {
+			nowIdx++;
+		} else {
+			nowIdx = 0;
+		}
+
+		move();
+	});
+});
